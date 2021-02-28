043c92c637772175c801f8fddc1e409ba583f908
loyo <1963597184@qq.com>
2021-02-28 14:54:51 +0800

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../components/Login.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+
+]
+
+const router = new VueRouter({
+	routes: [
+		{
+			path: "/",
+			redirect: '/login'
+		},
+		{
+			path: "/login",
+			component: Login
+		}
+	]
+})
+
+export default router
